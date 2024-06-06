0e7cccf9994c678b680bcd4b3cce79c9a5d700f2
filip7557 <filip.curic@student.ferit.hr>
2024-06-06 22:36:29 +0200

    Finish register screen

--- app/src/main/java/hr/ferit/filipcuric/conferencio/ui/register/RegisterScreen.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/register/RegisterScreen.kt
@@ -4,21 +4,26 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import hr.ferit.filipcuric.conferencio.data.repository.UserRepositoryImpl
 import hr.ferit.filipcuric.conferencio.ui.component.BackButton
 import hr.ferit.filipcuric.conferencio.ui.component.BlueButton
 import hr.ferit.filipcuric.conferencio.ui.component.TextBox
 import hr.ferit.filipcuric.conferencio.ui.theme.Blue
 
-@Preview
 @Composable
-fun RegisterScreen() {
+fun RegisterScreen(
+    viewModel: RegisterViewModel,
+) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -37,12 +42,48 @@ fun RegisterScreen() {
         ) {
             Title()
             Subtitle()
-            TextBox(label = "Full name", value = "", onValueChange = {})
-            TextBox(label = "Company/Organization", value = "", onValueChange = {})
-            TextBox(label = "Position", value = "", onValueChange = {})
-            TextBox(label = "Email", value = "", onValueChange = {})
-            TextBox(label = "Password", value = "", onValueChange = {})
-            BlueButton(text = "Register", onClick = {})
+            TextBox(
+                label = "Full name",
+                value = viewModel.fullname,
+                onValueChange = {
+                    viewModel.onFullnameChange(it)
+                }
+            )
+            TextBox(
+                label = "Company/Organization",
+                value = viewModel.company,
+                onValueChange = {
+                    viewModel.onCompanyChange(it)
+                }
+            )
+            TextBox(
+                label = "Position",
+                value = viewModel.position,
+                onValueChange = {
+                    viewModel.onPositionChange(it)
+                }
+            )
+            TextBox(
+                label = "Email",
+                value = viewModel.email,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                ),
+                onValueChange = {
+                    viewModel.onEmailChange(it)
+                }
+            )
+            TextBox(
+                label = "Password",
+                value = viewModel.password,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation(),
+                onValueChange = {
+                    viewModel.onPasswordChange(it)
+                })
+            BlueButton(text = "Register", onClick = { viewModel.onRegisterClick() })
         }
     }
 }
@@ -64,4 +105,10 @@ fun Subtitle() {
         modifier = Modifier
             .padding(bottom = 50.dp)
     )
+}
+
+@Preview
+@Composable
+fun RegisterScreenPreview() {
+    RegisterScreen(viewModel = RegisterViewModel(UserRepositoryImpl()))
 }
