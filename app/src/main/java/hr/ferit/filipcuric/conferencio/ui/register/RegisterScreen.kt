211c89a7bdea7df55dcec7659ddf6daf4c37fd6a
filip7557 <filip.curic@student.ferit.hr>
2024-06-06 16:56:48 +0200

    Finish register screen

--- app/src/main/java/hr/ferit/filipcuric/conferencio/ui/register/RegisterScreen.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/register/RegisterScreen.kt
@@ -2,26 +2,48 @@ package hr.ferit.filipcuric.conferencio.ui.register
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import hr.ferit.filipcuric.conferencio.ui.component.BackButton
+import hr.ferit.filipcuric.conferencio.ui.component.BlueButton
+import hr.ferit.filipcuric.conferencio.ui.component.TextBox
 import hr.ferit.filipcuric.conferencio.ui.theme.Blue
 
+@Preview
 @Composable
 fun RegisterScreen() {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 10.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .fillMaxSize()
+            .padding(10.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start
     ) {
-        Title()
+        BackButton(
+            onClick = {}
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Title()
+            Subtitle()
+            TextBox(label = "Full name", value = "", onValueChange = {})
+            TextBox(label = "Company/Organization", value = "", onValueChange = {})
+            TextBox(label = "Position", value = "", onValueChange = {})
+            TextBox(label = "Email", value = "", onValueChange = {})
+            TextBox(label = "Password", value = "", onValueChange = {})
+            BlueButton(text = "Register", onClick = {})
+        }
     }
 }
 
@@ -32,4 +54,14 @@ fun Title() {
         fontSize = 48.sp,
         color = Blue,
         )
+}
+
+@Composable
+fun Subtitle() {
+    Text(
+        text = "Create your new account",
+        fontSize = 20.sp,
+        modifier = Modifier
+            .padding(bottom = 50.dp)
+    )
 }
