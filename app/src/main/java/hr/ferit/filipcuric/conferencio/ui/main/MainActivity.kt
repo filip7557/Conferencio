32517f060cbb1661d9eafa43c01c4dbc88c5b61a
filip7557 <filip.curic@student.ferit.hr>
2024-06-06 16:57:35 +0200

    Remove unneeded function calls

--- app/src/main/java/hr/ferit/filipcuric/conferencio/ui/main/MainActivity.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/main/MainActivity.kt
@@ -10,37 +10,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import hr.ferit.filipcuric.conferencio.ui.theme.ConfferencioTheme
+import hr.ferit.filipcuric.conferencio.ui.register.RegisterScreen
+import hr.ferit.filipcuric.conferencio.ui.theme.ConferencioTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ConfferencioTheme {
+            ConferencioTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ConfferencioTheme {
-        Greeting("Android")
     }
 }
