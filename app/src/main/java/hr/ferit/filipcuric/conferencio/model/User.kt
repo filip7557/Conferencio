0d29a85f498144e98afb7a5bae07d1b1377bc1b3
filip7557 <filip.curic@student.ferit.hr>
2024-06-07 00:03:34 +0200

    Add image url property to user model

--- app/src/main/java/hr/ferit/filipcuric/conferencio/model/User.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/model/User.kt
@@ -2,6 +2,7 @@ package hr.ferit.filipcuric.conferencio.model
 
 data class User(
     var id: String? = null,
+    var imageUrl: String = "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg",
     val fullname: String = "",
     val company: String = "",
     val position: String = "",
