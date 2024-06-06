ee7bce08271ce11fe0065ff3ba18fe9c9f33ea48
filip7557 <filip.curic@student.ferit.hr>
2024-06-06 16:54:55 +0200

    Add back button

---
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/component/BackButton.kt
@@ -0,0 +1,40 @@
+package hr.ferit.filipcuric.conferencio.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import hr.ferit.filipcuric.conferencio.R
+import hr.ferit.filipcuric.conferencio.ui.theme.DarkTertiaryColor
+import hr.ferit.filipcuric.conferencio.ui.theme.TertiaryColor
+
+@Composable
+fun BackButton(
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .clip(CircleShape)
+            .size(32.dp)
+            .background(if(isSystemInDarkTheme()) DarkTertiaryColor else TertiaryColor)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.baseline_arrow_back_ios_new_24),
+            contentDescription = "back icon",
+            modifier = Modifier
+                .padding(start = 2.5.dp, top=4.5.dp))
+    }
+}
