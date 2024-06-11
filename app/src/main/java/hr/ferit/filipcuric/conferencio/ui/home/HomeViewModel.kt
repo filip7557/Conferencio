f09e3c1a1970654d50c0a54f6212704935aae982
filip7557 <filip.curic@student.ferit.hr>
2024-06-11 16:11:51 +0200

    Add home view model

--- app/src/main/java/hr/ferit/filipcuric/conferencio/ui/home/HomeViewModel.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/home/HomeViewModel.kt
@@ -2,9 +2,11 @@ package hr.ferit.filipcuric.conferencio.ui.home
 
 import androidx.lifecycle.ViewModel
 import hr.ferit.filipcuric.conferencio.data.repository.ConferenceRepository
+import hr.ferit.filipcuric.conferencio.data.repository.UserRepository
 
 class HomeViewModel(
     private val conferenceRepository: ConferenceRepository,
+    private val userRepository: UserRepository,
 ) : ViewModel() {
 
 }
