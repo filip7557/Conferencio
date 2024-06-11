3aaa261c0a54811b322ade5b70960debac3da277
filip7557 <filip.curic@student.ferit.hr>
2024-06-11 16:11:00 +0200

    Change how datetime is stored

--- app/src/main/java/hr/ferit/filipcuric/conferencio/model/Conference.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/model/Conference.kt
@@ -6,6 +6,7 @@ data class Conference(
     var id: String? = null,
     val imageUrl: String = "https://firebasestorage.googleapis.com/v0/b/conferencio-57027.appspot.com/o/conference_banners%2Fdefault_banner.jpg?alt=media&token=8cb443b7-cf23-4aae-885b-520808c31cdd",
     val title: String = "",
-    val startDateTime: DateTime = DateTime.getDefaultInstance(),
-    val endDateTime: DateTime = DateTime.getDefaultInstance(),
+    val startDateTime: Long,
+    val endDateTime: Long,
+    val ownerId: String = "",
 )
