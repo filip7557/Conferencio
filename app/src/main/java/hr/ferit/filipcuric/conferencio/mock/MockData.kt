9a73c8fabc4d135b7fd122bf643411b040a2389c
filip7557 <filip.curic@student.ferit.hr>
2024-06-11 16:11:13 +0200

    Add mock data

---
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/mock/MockData.kt
@@ -0,0 +1,21 @@
+package hr.ferit.filipcuric.conferencio.mock
+
+import com.google.type.DateTime
+import hr.ferit.filipcuric.conferencio.model.Conference
+
+fun getConferences() = listOf(
+    Conference(
+        imageUrl = "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F784276859%2F1352392056833%2F1%2Foriginal.20240606-111051?w=940&auto=format%2Ccompress&q=75&sharp=10&s=1aa5f4e7837ee73bd158c319936fd26d",
+        title = "NowInMobile Meetup",
+        startDateTime = 1718895600000,
+        endDateTime = 1719000000000,
+        ownerId = "SNbVzHkToCRgnDmZWfmddQZeAsy2"
+    ),
+    Conference(
+        imageUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTgRO4qcSMER2GGFGeNqnqHPink32-c_H7Ng&s",
+        title = "Dovik@FERIT",
+        startDateTime = 1718913600000,
+        endDateTime = 1719345600000,
+        ownerId = "qgTZb0vTpddc7eI88l4PZNPqwn22"
+    )
+)
