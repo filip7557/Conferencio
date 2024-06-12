aea20428c1848f3918124127253c8decab57fefe
filip7557 <filip.curic@student.ferit.hr>
2024-06-12 22:27:55 +0200

    Change mock  data

--- app/src/main/java/hr/ferit/filipcuric/conferencio/mock/MockData.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/mock/MockData.kt
@@ -24,8 +24,8 @@ fun getConferences() = listOf(
         id = "3",
         imageUrl = "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F784276859%2F1352392056833%2F1%2Foriginal.20240606-111051?w=940&auto=format%2Ccompress&q=75&sharp=10&s=1aa5f4e7837ee73bd158c319936fd26d",
         title = "NowInMobile Meetup",
-        startDateTime = 1718895600000,
-        endDateTime = 1719000000000,
+        startDateTime = 1716616800000,
+        endDateTime = 1717048800000,
         ownerId = "SNbVzHkToCRgnDmZWfmddQZeAsy2"
     ),
 )
