ce11fbbb6ac281386b4e355396701783313176ef
filip7557 <filip.curic@student.ferit.hr>
2024-06-26 18:48:01 +0200

    Fix crash on owners not loading

--- app/src/main/java/hr/ferit/filipcuric/conferencio/ui/browse/BrowseScreen.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/browse/BrowseScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import hr.ferit.filipcuric.conferencio.data.repository.ConferenceRepositoryImpl
 import hr.ferit.filipcuric.conferencio.data.repository.UserRepositoryImpl
+import hr.ferit.filipcuric.conferencio.model.User
 import hr.ferit.filipcuric.conferencio.navigation.ConferenceDestination
 import hr.ferit.filipcuric.conferencio.ui.component.ConferenceCard
 
@@ -48,9 +49,10 @@ fun BrowseScreen(
                 items = conferences.value,
                 key = { conference -> conference.id!! }
             ) {
+
                 ConferenceCard(
                     conference = it,
-                    user = owners.value[conferences.value.indexOf(it)],
+                    user = if (owners.value.size < conferences.value.size) User() else owners.value[conferences.value.indexOf(it)],
                     onClick = { onConferenceClick(ConferenceDestination.createNavigation(it.id!!)) }
                 )
             }
