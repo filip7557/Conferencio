e014aaf96eb2cc3267397ec6c2228b1848fe9a6d
filip7557 <filip.curic@student.ferit.hr>
2024-08-11 18:04:46 +0200

    Add picture component

---
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/component/Picture.kt
@@ -0,0 +1,30 @@
+package hr.ferit.filipcuric.conferencio.ui.component
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import hr.ferit.filipcuric.conferencio.model.Picture
+
+@Composable
+fun Picture(
+    picture: Picture
+) {
+    val context = LocalContext.current
+    AsyncImage(
+        model = picture.imageUrl,
+        contentDescription = "picture",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(180.dp)
+            .clickable {
+                context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(picture.imageUrl)))
+            }
+    )
+}
