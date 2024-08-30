7ad99268956adfcf2b4390670018e67bf5642e28
filip7557 <filip.curic@student.ferit.hr>
2024-08-30 21:59:42 +0200

    Remove unsused colors

--- app/src/main/java/hr/ferit/filipcuric/conferencio/ui/theme/Color.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/ui/theme/Color.kt
@@ -3,13 +3,6 @@ package hr.ferit.filipcuric.conferencio.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Blue = Color(52, 93, 168)
-val DarkBlue = Color(40, 78, 148)
-
-/*val DarkTertiaryColor = Color(0.75294f, 0.75294f, 0.75294f, 0.1f)
-val DarkOnTertiaryColor = Color(1f, 1f, 1f, 0.6f)
-
-val TertiaryColor = Color(0f, 0f, 0f, 0.08f)
-val OnTertiaryColor = Color(0f, 0f, 0f, 0.6f)*/
 
 val DarkTertiaryColor = Color(55, 55, 56)
 val DarkOnTertiaryColor = Color(146, 146, 148)
