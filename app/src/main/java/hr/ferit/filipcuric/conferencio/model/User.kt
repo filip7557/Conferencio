8ba2dd3ca82a7b25d76ca7b43ea64ed75b75d660
filip7557 <filip.curic@student.ferit.hr>
2024-09-03 13:09:49 +0200

    Add default value for null user

--- app/src/main/java/hr/ferit/filipcuric/conferencio/model/User.kt
+++ app/src/main/java/hr/ferit/filipcuric/conferencio/model/User.kt
@@ -3,7 +3,7 @@ package hr.ferit.filipcuric.conferencio.model
 data class User(
     var id: String? = null,
     var imageUrl: String = "https://firebasestorage.googleapis.com/v0/b/conferencio-57027.appspot.com/o/profile_pictures%2Fdefault_profile.jpg?alt=media&token=f955d74f-47c1-4199-aba9-fb3366070b67",
-    val fullname: String = "",
+    val fullname: String = "Deleted User",
     val company: String = "",
     val position: String = "",
     val email: String = "",
